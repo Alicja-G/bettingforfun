06c24190384fbc7e8a08fd27bde3553af11bab0b
Alicja Gadecka <gadecka.alicja@gmail.com>
2021-12-23 23:02:07 +0100

    #1 architecture

---
+++ BettingForFun.BLUnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace BettingForFun.BLUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
